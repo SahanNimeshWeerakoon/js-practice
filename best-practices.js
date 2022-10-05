b8a92b703e42b4a187b87a2e8c62c39f49a36838
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2022-10-05 08:24:09 +0530

    deep nesting

--- best-practices.js
+++ best-practices.js
@@ -1,6 +1,18 @@
 // Magic numbers = numbers we assign with no clear meaning
-const SECONDS_IN_A_DAY = 86400;
+// const SECONDS_IN_A_DAY = 86400;
 
-for (let i=0; i<SECONDS_IN_A_DAY; i++) {
-    // do smt
+// for (let i=0; i<SECONDS_IN_A_DAY; i++) {
+//     // do smt
+// }
+
+// Deep nesting
+const exampleArr = [[['value']]];
+
+function retrieveFinalElement(element) {
+    if (Array.isArray(element)) {
+        return retrieveFinalElement(element[0]);
+    }
+    return element;
 }
+
+retrieveFinalElement(exampleArr);
