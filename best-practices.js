ee4499cf5565c4436195997d7db08b4b14a6d48b
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2022-10-05 08:40:05 +0530

    dnt keep multiple functionalities in single function

--- best-practices.js
+++ best-practices.js
@@ -5,14 +5,46 @@
 //     // do smt
 // }
 
-// Deep nesting
-const exampleArr = [[['value']]];
+// ============================================================
 
-function retrieveFinalElement(element) {
-    if (Array.isArray(element)) {
-        return retrieveFinalElement(element[0]);
-    }
-    return element;
+// Deep nesting
+// const exampleArr = [[['value']]];
+
+// function retrieveFinalElement(element) {
+//     if (Array.isArray(element)) {
+//         return retrieveFinalElement(element[0]);
+//     }
+//     return element;
+// }
+
+// retrieveFinalElement(exampleArr);
+
+// ============================================================
+
+// Stop writing comments - code must speak for it self
+
+// ============================================================
+
+// Avoid large functions
+
+const addMultiSubs = (a,b,c) => {
+    // If a function have multiple functionalities, dnt do them inside same function. 
+    // Break them to multiple functions
+    // const addition = a + b + c;
+    // const multiply = a * b * c;
+    // const substract = a - b - c;
+
+    const addition = add(a,b,c);
+    const multiply = multiply(a,b,c);
+    const substract = substract(a,b,c);
+    
+    return `${addition} ${multiply} ${substract}`;
 }
 
-retrieveFinalElement(exampleArr);
+const add = (a,b,c) =>  a + b + c;
+const multiply = (a,b,c) => a * b * c;
+const substract = (a,b,c) => a - b - c;
+
+// ==============================================================
+
+
